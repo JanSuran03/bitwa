b561c421e90fd14f33fbca822680896e92b1a37e
Jan Šuráň <suran.orgpad@gmail.com>
2023-12-28 12:06:04 +0100

    Add user table rendering.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED')]
 class UsersController extends AbstractController {
+    public UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository) {
+        $this->userRepository = $userRepository;
+    }
     #[Route('/users', name: 'app_users')]
     public function login(): Response {
-        return $this->render('users.html.twig');
+        return $this->render('users.html.twig',
+            ['users' => $this->userRepository->findAll()]);
     }
 }
