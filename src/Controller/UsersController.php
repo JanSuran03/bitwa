ba083c9cfca0d4c8dff0f99b1f3d98d904a5e03b
Jiří Heller <hellejir@fit.cvut.cz>
2023-12-29 19:03:59 +0100

    Refactor Users page

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,14 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED')]
 class UsersController extends AbstractController {
-    public UserRepository $userRepository;
+    public UserService $userService;
 
-    public function __construct(UserRepository $userRepository) {
-        $this->userRepository = $userRepository;
+    public function __construct(UserService $userService) {
+        $this->userService = $userService;
     }
     #[Route('/users', name: 'app_users')]
     public function login(): Response {
         return $this->render('users.html.twig',
-            ['users' => $this->userRepository->findAll()]);
+            ['users' => $this->userService->getAll()]);
     }
 }
