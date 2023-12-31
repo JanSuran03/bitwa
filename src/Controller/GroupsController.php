5f74030292816ac37f2c5aeeff700a9820a21492
Jan Šuráň <suran.orgpad@gmail.com>
2023-12-31 22:26:17 +0100

    Add a base for groups view.

--- src/Controller/GroupsController.php
+++ src/Controller/GroupsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
 class GroupsController extends AbstractController {
+    private GroupRepository $groupRepository;
+
+    public function __construct(GroupRepository $groupRepository){
+        $this->groupRepository = $groupRepository;
+    }
     #[Route('/groups', name: 'app_groups')]
+    #[IsGranted('ROLE_USER')]
     public function groups(): Response {
-        return $this->render('groups.html.twig');
+        return $this->render('groups.html.twig',
+            ['groups' => $this->groupRepository->findAll()]
+        );
     }
 }
