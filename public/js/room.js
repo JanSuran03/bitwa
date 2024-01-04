2699483f03cfc015cf7be1c5ef2f388a398f5814
Jiří Heller <hellejir@fit.cvut.cz>
2024-01-04 15:21:39 +0100

    Move JS from HTML body to public/js

---
+++ public/js/room.js
@@ -0,0 +1,85 @@
+function reloadManagersTable() {
+    const roomId = document.getElementById('top-block').dataset.roomId
+    const managersTableBody = document.getElementById('managers-table-body')
+    fetch('/api/rooms/' + roomId + '/managers', {headers: {"Accept": "application/json"}})
+        .then(response => response.json())
+        .then(managers => {
+
+            managersTableBody.innerHTML = ''
+
+            if (managers.length === 0) {
+                const row = document.createElement('tr')
+                row.innerHTML = '<td colSpan="2">Žádný správce</td>'
+                managersTableBody.appendChild(row)
+            } else {
+                managers.forEach(manager => {
+                    const row = document.createElement('tr')
+                    row.innerHTML += `<td>${manager.name}</td>`
+                    row.innerHTML += `<td>
+                                                      <img class="wide a" src="/images/trash-red.svg" alt="Odebrat správce"
+                                                           onclick="deleteManagerFromRoom(${manager.id}, '${manager.name}')">
+                                                  </td>`
+                    managersTableBody.appendChild(row)
+                })
+            }
+        })
+}
+
+function deleteManagerFromRoom(managerId, managerName) {
+    const roomId = document.getElementById('top-block').dataset.roomId
+    const roomName = document.getElementById('top-block').dataset.roomName
+    if (confirm(`Skutečně chcete odebrat uživateli ${managerName} správcovská práva pro učebnu ${roomName}?`)) {
+        fetch(`/api/rooms/${roomId}/managers/${managerId}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {}
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(`Nepodařilo se odebrat uživateli ${managerName} správcovská práva pro učebnu ${roomName}: ${response.statusText}`);
+            }
+
+            if (response.status === 204) {
+                const newManagerSelect = document.getElementById("new-manager")
+                const newOption = document.createElement('option')
+                newOption.setAttribute("value", managerId)
+                newOption.innerHTML = managerName
+                newManagerSelect.appendChild(newOption)
+                return;
+            }
+
+            console.warn(`Špatný návratový kód: ${response.status}`);
+            return;
+        }).then(_ => {
+            console.log(`Uživateli ${managerName} byla úspěšně odebrána správcovská práva pro učebnu ${roomName}.`);
+            reloadManagersTable()
+        }).catch(error => {
+            console.error(`Chyba při odebírání správcovských práv uživatele ${managerName} pro učebnu ${roomName}: ${error.message}`)
+        })
+    }
+}
+
+function addRoomManager(roomId, managerId) {
+    return fetch(`/api/rooms/${roomId}/managers/${managerId}`, {method: 'PUT'})
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addManagerButton = document.getElementById('new-manager-save-button')
+    if (addManagerButton != null) {
+        addManagerButton.addEventListener('click', event => {
+            event.preventDefault()
+            const roomId = document.getElementById('top-block').dataset.roomId
+            const addManagerForm = document.getElementById('new-manager-form')
+            const newManagerData = new FormData(addManagerForm)
+            const newManagerId = newManagerData.get('new-manager')
+            const optionToRemove = document.querySelector(`option[value="${newManagerId}"]`)
+            optionToRemove.remove()
+            addRoomManager(roomId, newManagerId)
+                .then(() => reloadManagersTable())
+        })
+    }
+
+    reloadManagersTable()
+})
